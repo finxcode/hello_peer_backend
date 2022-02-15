2a2019db45d1557d4aa04aed4c8502c5063397ef
finxcode <f.sheng@outlook.com>
2022-02-15 17:46:27 +0800

    user model changed

--- app/models/user.go
+++ app/models/user.go
@@ -4,9 +4,15 @@ import "strconv"
 
 type User struct {
 	ID
-	Name     string `json:"name" gorm:"not null;comment:user name"`
-	Mobile   string `json:"mobile" gorm:"not null;index;comment:user phone"`
-	Password string `json:"password" gorm:"not null;default:'';comment:user password"`
+	Name       string  `json:"name" gorm:"not null;comment:用户名"`
+	Mobile     string  `json:"mobile" gorm:"not null;index;comment:手机号"`
+	Password   string  `json:"password" gorm:"not null;default:'';comment:密码"`
+	Gender     string  `json:"gender" gorm:"not null;default:'';comment:性别"`
+	NickName   string  `json:"nickName" gorm:"not null;default:'';comment:微信用户名"`
+	Age        int     `json:"age" gorm:"not null;default:18;comment:年龄"`
+	Occupation string  `json:"occupation" gorm:"comment:职业"`
+	Lat        float32 `json:"lat" gorm:"comment:维度"`
+	Lng        float32 `json:"lng" gorm:"comment:经度"`
 	Timestamps
 	SoftDeletes
 }
