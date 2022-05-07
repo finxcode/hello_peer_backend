6caf81c25ea7bb7f8d2be3bd0fb6cda91b5e71d0
finxcode <f.sheng@outlook.com>
2022-05-07 11:45:15 +0800

    building square user finder +2

--- app/services/query.go
+++ app/services/query.go
@@ -17,7 +17,7 @@ func MakeSquareQueryIn3Day(uid int, limit int, sq SquareSetting) (string, error,
 	}
 	startTime := date.GetDateByOffsetDay(-3, time.Now())
 	endTime := date.GetDateByOffsetDay(0, time.Now())
-	resQuery = fmt.Sprintf("id != %s and created_at > %s and created_at < %s and %s order by rand() limit %s", strconv.Itoa(uid), startTime, endTime, rule, strconv.Itoa(limit))
+	resQuery = fmt.Sprintf("id != %s and created_at > '%s' and created_at < '%s' and %s order by rand() limit %s", strconv.Itoa(uid), startTime, endTime, rule, strconv.Itoa(limit))
 
 	return resQuery, nil, 0
 
@@ -30,6 +30,6 @@ func MakeSquareQueryBefore3Day(uid int, limit int, sq SquareSetting) (string, er
 		return "", errors.New("广场条件设置错误"), errorCode
 	}
 	endTime := date.GetDateByOffsetDay(-3, time.Now())
-	resQuery = fmt.Sprintf("id != %s and created_at  < %s and %s order by rand() limit %s", strconv.Itoa(uid), endTime, rule, strconv.Itoa(limit))
+	resQuery = fmt.Sprintf("id != %s and created_at  < '%s' and '%s' order by rand() limit %s", strconv.Itoa(uid), endTime, rule, strconv.Itoa(limit))
 	return resQuery, nil, 0
 }
