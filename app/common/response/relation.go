effb5cff82cecb769a996f84f1aac7516c9a00fd
finxcode <f.sheng@outlook.com>
2022-08-31 17:21:28 +0800

    building fans list +11

--- app/common/response/relation.go
+++ app/common/response/relation.go
@@ -5,12 +5,12 @@ type MyFans struct {
 }
 
 type Fan struct {
-	Id         int    `json:"uid" gorm:"column:id"`
-	UserName   string `json:"username" gorm:"column:user_name"`
+	Id         int    `json:"uid" gorm:"-"`
+	UserName   string `json:"username" gorm:"-"`
 	PetName    string `json:"petName" gorm:"-"`
-	Age        int    `json:"age" gorm:"column:age"`
-	Location   string `json:"location" gorm:"column:location"`
-	Occupation string `json:"occupation" gorm:"column:occupation"`
-	Images     string `json:"coverImage" gorm:"column:images"`
+	Age        int    `json:"age" gorm:"-"`
+	Location   string `json:"location" gorm:"-"`
+	Occupation string `json:"occupation" gorm:"-"`
+	Images     string `json:"coverImage" gorm:"-"`
 	//Status     string `json:"status"`
 }
