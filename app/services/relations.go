eea32b5a9cea46c9bba9878492a9ef6dd025a9e7
finxcode <f.sheng@outlook.com>
2022-08-31 16:48:45 +0800

    building fans list +5

--- app/services/relations.go
+++ app/services/relations.go
@@ -102,7 +102,7 @@ func (r *relationService) GetFans(uid int) (*[]response.MyFans, int, error) {
 	}
 
 	err := global.App.DB.Table("wechat_users").
-		Select("wechat_users.id as uid, wechat_users.user_name, pets.pet_name, wechat_users.age, wechat_users.location,"+
+		Select("wechat_users.id, wechat_users.user_name, pets.pet_name, wechat_users.age, wechat_users.location,"+
 			"wechat_users.occupation, wechat_users.images").
 		Joins("inner join pets on wechat_users.id = pets.user_id").
 		Joins("inner join focus_ons on focus_ons.focus_to = wechat_users.id").
@@ -110,7 +110,7 @@ func (r *relationService) GetFans(uid int) (*[]response.MyFans, int, error) {
 		Scan(&fans).Error
 
 	if err != nil {
-		zap.L().Error("database error", zap.String("looking for fans error", res.Error.Error()))
+		zap.L().Error("database error", zap.String("looking for fans error", err.Error()))
 		return nil, -1, errors.New("looking for fans DB error")
 	}
 
