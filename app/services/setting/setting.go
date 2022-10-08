b52645f3f6c3ba423fe479bf1af22868f93243e4
finxcode <f.sheng@outlookcom>
2022-10-08 22:01:16 +0800

    fix getUserSettings Api sql query bug +1

--- app/services/setting/setting.go
+++ app/services/setting/setting.go
@@ -18,8 +18,7 @@ func (s *settingService) GetUserSysSetting(uid int) (*dto.UserSetting, error) {
 	var userSetting dto.UserSetting
 
 	err := global.App.DB.Table("wechat_users").
-		Select("distinct(wechat_users.id), wechat_users.verified, pets.verified, wechat_users.hello_id,"+
-			"wechat_users.mobile, wechat_users.wechat_name").
+		Select("distinct(wechat_users.id), wechat_users.verified, pets.verified, wechat_users.hello_id, wechat_users.mobile, wechat_users.wechat_name").
 		Joins("inner join pets on wechat_users.id = pets.user_id").
 		Where("wechat_users.id = ?", uid).
 		Scan(&userSetting).Error
