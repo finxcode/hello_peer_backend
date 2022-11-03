e44da6a9a44702d1bd0dd880c6b89a21b80be9ca
finxcode <f.sheng@outlook.com>
2022-11-03 17:17:37 +0800

    migrate to new server +2 debugging

--- app/models/pet.go
+++ app/models/pet.go
@@ -10,8 +10,8 @@ type Pet struct {
 	Sex         string  `gorm:"comment:宠物性别"`
 	Birthday    string  `gorm:"comment:宠物生日"`
 	Weight      float32 `gorm:"comment:宠物体重"`
-	Description string  `gorm:"type:varchar(1024) comment:宠物描述"`
-	Images      string  `gorm:"type:varchar(1024) comment:宠物图片"`
+	Description string  `gorm:"type:varchar(1024); comment:宠物描述"`
+	Images      string  `gorm:"type:varchar(1024); comment:宠物图片"`
 	Verified    int     `gorm:"comment:宠物是否认证"`
 	Timestamps
 	SoftDeletes
